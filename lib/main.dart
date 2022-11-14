80a68b359ec8e4aca3972e6683a93adb5b50c2ec
Shakib Hossen <64243974+xaakib@users.noreply.github.com>
2022-11-14 13:07:34 +0600

    color changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:ortho_songbad/screens/home_screen.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
-  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatefulWidget {
@@ -80,72 +77,73 @@ class _OnBoardScrenState extends State<OnBoardScren> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 255, 255, 255),
-        body: Container(
-          alignment: Alignment.center,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  child: Image.asset(
-                    "assets/images/icon.png",
-                    height: 100,
-                    width: 100,
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                const SizedBox(height: 8),
-                Container(
-                  child: Image.asset(
-                    "assets/images/cover.png",
-                    height: 100,
-                    fit: BoxFit.fitWidth,
-                  ),
-                ),
-                const SizedBox(height: 200),
-                Card(
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        const Text(
-                          "Select your language",
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                        const SizedBox(height: 16),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Material(
-                              child: TextButton(
-                                  onPressed: () {
-                                    Get.offAll(MyHomePage(
-                                        url: "https://orthosongbad.com/"));
-                                    selectLanguage("Bangla");
-                                  },
-                                  child: Text("Bangla")),
-                            ),
-                            Material(
-                              child: TextButton(
-                                  onPressed: () {
-                                    Get.offAll(MyHomePage(
-                                        url: "https://en.orthosongbad.com/"));
-                                    selectLanguage("English");
-                                  },
-                                  child: Text("English")),
-                            ),
-                          ],
-                        )
-                      ],
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Positioned(
+                top: 100,
+                left: 10,
+                right: 10,
+                child: Column(
+                  children: [
+                    Container(
+                      child: Image.asset(
+                        "assets/images/icon.png",
+                        height: 100,
+                        width: 100,
+                        fit: BoxFit.fill,
+                      ),
                     ),
-                  ),
-                )
-              ],
-            ),
+                    const SizedBox(height: 8),
+                    Container(
+                      child: Image.asset(
+                        "assets/images/cover.png",
+                        height: 100,
+                        fit: BoxFit.fitWidth,
+                      ),
+                    ),
+                    const SizedBox(height: 200),
+                  ],
+                ),
+              ),
+              Positioned(
+                bottom: 100,
+                left: 10,
+                right: 10,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      height: 45,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Get.offAll(const MyHomePage(
+                                url: "https://orthosongbad.com/"));
+                            selectLanguage("Bangla");
+                          },
+                          child: const Text("Bangla",
+                              style: TextStyle(fontSize: 18))),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 45,
+                      width: double.infinity,
+                      child: ElevatedButton(
+                          onPressed: () {
+                            Get.offAll(const MyHomePage(
+                                url: "https://en.orthosongbad.com/"));
+                            selectLanguage("English");
+                          },
+                          child: const Text("English",
+                              style: TextStyle(fontSize: 18))),
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
